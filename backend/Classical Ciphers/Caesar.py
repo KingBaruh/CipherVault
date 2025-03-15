9ec3b3ec03f01b7c65fe4cf613c4eed7539090f8
Baruh <baruhriss@gmail.com>
2025-03-15 08:24:38 +0200

    Finish Caesar

--- backend/Classical Ciphers/Caesar.py
+++ backend/Classical Ciphers/Caesar.py
@@ -2,6 +2,7 @@ from abc import ABC
 
 from backend.Cipher import Cipher
 
+
 class CaesarCipher(Cipher):
     def __init__(self, key):
         self.key = key
@@ -13,9 +14,25 @@ class CaesarCipher(Cipher):
         self.key = key
 
     def encrypt(self, text: str) -> str:
-        """Encrypt the given text"""
-        pass
+        return self.coded(text, self.key)
 
     def decrypt(self, text: str) -> str:
-        """Decrypt the given text"""
-        pass
+        return self.coded(text, 26 - self.key)
+
+    def coded(self, text: str, key) -> str:
+        result = ''
+        for char in text:
+
+            if char.isalpha():
+                # Encrypt/Dcrypt uppercase characters
+                if char.isupper():
+                    result += chr((ord(char) + key - 65) % 26 + 65)
+
+                # Encrypt/Dcrypt lowercase characters
+                else:
+                    result += chr((ord(char) + key - 97) % 26 + 97)
+            else:
+                result += char
+
+        return result
+
